cb223eeb77d814bd601c9fd2fcbd524010b6ee39
YatharthChauhan2362 <yc232326@gmail.com>
2025-01-13 21:10:19 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -88,11 +88,11 @@ export default function Login() {
           </motion.button>
         </div>
 
-        <div className="text-sm text-center">
+        {/* <div className="text-sm text-center">
           <Link to="/signup" className="font-medium text-blue-600 hover:text-blue-500">
             Don't have an account? Sign up
           </Link>
-        </div>
+        </div> */}
       </form>
     </AuthLayout>
   );
